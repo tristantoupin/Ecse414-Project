f04a1ecda9be7b5131c3e7fb9264395b8f5a934a
tristantoupin <tristantoupin@gmail.com>
2017-10-31 11:42:55 -0400

    temp modifiacation, not working yet

--- script/traceroute.sh
+++ script/traceroute.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-LINKS=("unsw.edu.au" "unisa.ac.za" "mcgill.ca")
+LINKS=("8.8.8.8" "unimelb.edu.au" "caltech.edu")
 UNI_LIST=(unsw unisa mcgill)
 INDEX=1
 
 for link in "${LINKS[@]}"
 do
 	: 
+	START=$(date +%s)
+	echo $START
 	echo $INDEX
 	echo "$link"
-	#traceroute $link
+	traceroute $link
 	echo "`date`, google, toupin" >> log.csv
 	((INDEX++))
+
+	END=$(date +%s)
+	DIFF=$(($END-$START))
+	echo $START
+	echo $END
+	echo "It took $DIFF seconds"
 done
+
+
