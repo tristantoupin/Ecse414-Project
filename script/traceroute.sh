452e677a1ce6d9986f34924f490630e7877e868a
tristantoupin <tristantoupin@gmail.com>
2017-11-08 13:09:45 -0500

    complete text files

--- script/traceroute.sh
+++ script/traceroute.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-LINKS=("8.8.8.8" "unimelb.edu.au" "caltech.edu")
-UNI_LIST=(unsw unisa mcgill)
-INDEX=1
+LINKS=("130.102.82.61" "138.44.176.3" "112.137.142.4" "124.124.195.101" "155.232.32.14" "80.239.135.226" "84.237.50.25" "198.154.248.116" "143.107.249.34" "128.171.213.2" "188.44.50.103" "130.235.52.5" "194.199.156.25" "192.76.32.66" "132.247.70.37" "137.82.123.113" "128.227.9.98" "128.100.96.19" "132.216.177.160")
+MAXHOPS=25
+CP_TOBACKUP_INDEX=0
+BACKUP_NUMBER=0
 
-for link in "${LINKS[@]}"
+while true
 do
-	: 
-	START=$(date +%s)
-	echo $START
-	echo $INDEX
-	echo "$link"
-	traceroute $link
-	echo "`date`, google, toupin" >> log.csv
-	((INDEX++))
 
-	END=$(date +%s)
-	DIFF=$(($END-$START))
-	echo $START
-	echo $END
-	echo "It took $DIFF seconds"
+	for link in "${LINKS[@]}"
+	do
+		: 
+		LOCALCOMMAND="traceroute -m$MAXHOPS $link"
+		TEMPTRACEROUTE=`($LOCALCOMMAND)`
+		
+		#echo "$TEMPTRACEROUTE"
+		echo "Start time: " `date` >> temp_$link.txt
+		echo "$TEMPTRACEROUTE"  >> temp_$link.txt
+		echo "End time: " `date` >> temp_$link.txt
+		echo "\n\n" >> temp_$link.txt
+		sleep 2s
+	done
+
+#	if($CP_TOBACKUP_INDEX -eq 6) #every 12h do a backup of files
+#	then
+#		`mkdir ../backup_$BACKUP_NUMBER`
+#		`cp -r ./ ../backup_$BACKUP_NUMBER`
+#		`CP_TOBACKUP_INDEX=0`
+#	fi
+#	`let CP_TOBACKUP_INDEX++`
+
+	git add .
+
+	sleep 6600 #sleep 1h50mins
 done
-
 
